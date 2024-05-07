bc338fd1ca0b89d9773abbafead126d1bb5b13b0
Jasiek Kalęba <93594746+jkaleba@users.noreply.github.com>
2024-05-07 16:45:09 +0200

    Sending, catching signals etc.

---
+++ signals/catcher.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+
+#define EXIT_MODE 3
+
+int changed_work_mode = 0;
+int current_work_mode = -1;
+
+void handler(int sig, siginfo_t *info, void *context) {
+    if (sig == SIGUSR1) {
+        pid_t sender_pid = info->si_pid;
+        union sigval val = info->si_value;
+        int work_mode = val.sival_int;
+
+        if (work_mode != current_work_mode) {
+            current_work_mode = work_mode;
+            changed_work_mode++;
+        }
+
+        if (work_mode == EXIT_MODE) {
+            printf("Received mode 3. Exiting\n");
+            kill(sender_pid, SIGUSR1);
+            exit(0);
+        } else if (work_mode == 1) {
+            for (int i = 1; i <= 100; ++i) {
+                printf("%d\n", i);
+            }
+        } else if (work_mode == 2) {
+            printf("Changed work mode %d times\n", changed_work_mode);
+        } else {
+            printf("Invalid work mode: %d\n", work_mode);
+        }
+
+        kill(sender_pid, SIGUSR1);
+    }
+}
+
+int main() {
+    struct sigaction sa;
+
+    sa.sa_sigaction = handler;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGUSR1, &sa, NULL);
+
+    printf("Catcher PID: %d\n", getpid());
+
+    while (1) {
+        pause();
+    }
+
+    return 0;
+}
---
+++ signals/sender.c
@@ -0,0 +1,49 @@
+//
+// Created by Michał Kawa on 11/04/2024.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
+#define MAX_WORK_MODE 3
+
+pid_t catcher_pid;
+int work_mode;
+int success = 1;
+
+
+void handler(int sig) {
+    success = 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <catcher_pid> <work_mode>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    signal(SIGUSR1, handler);
+
+    catcher_pid = atoi(argv[1]);
+    work_mode = atoi(argv[2]);
+
+    struct sigaction act;
+    act.sa_handler = handler;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGUSR1, &act, NULL);
+
+
+    union sigval value;
+    value.sival_int = work_mode;
+    sigqueue(catcher_pid, SIGUSR1, value);
+
+
+    while (success);
+
+    printf("Confirmation received.\n");
+
+    return 0;
+}
